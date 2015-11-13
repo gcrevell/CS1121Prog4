f690a9751cd9d785a9067187662eb8aee3c9c606
Gabriel Revells <gcrevell@mtu.edu>
2015-11-13 11:09:50 -0500

    new Test case and changed toString in Queue.java

--- Queue.java
+++ Queue.java
@@ -94,17 +94,65 @@ public class Queue<T> implements QueueInterface<T> {
 	 * @return the string representation of the queue
 	 */
 	public String toString() {
-		String ret = "[";
+		String ret = "";
 		String flag = "";
 		Node<T> cur = head;
 		
 		while (cur != null) {
-			ret += flag + cur.getValue();
+			ret = cur.getValue() + flag + ret;
 			flag = ", ";
 			cur = cur.getNext();
 		}
 		
 		return ret + "]";
+	}
+	
+	public static void main(String args[]) throws Exception {
+		Queue<Integer> q = new Queue<Integer>(10);
+		
+		System.out.println("Size (Should be 0) = " + q.size());
+		System.out.println("Q is empty (should be true) " + q.isEmpty());
+		
+		q.enqueue(5);
+		
+		System.out.println("Size (Should be 1) = " + q.size());
+		System.out.println("Q is empty (should be false) " + q.isEmpty());
+		System.out.println("Q is full (should be false) " + q.isFull());
+		
+		System.out.println("Polling top value (Should be 5) " + q.peek());
+		System.out.println("Removing top value (Should be 5) " + q.dequeue());
+		
+		System.out.println("Size (Should be 0) = " + q.size());
+		System.out.println("Q is empty (should be true) " + q.isEmpty());
+		System.out.println("Q is full (should be false) " + q.isFull());
+		
+		q.enqueue(1);
+		q.enqueue(2);
+		q.enqueue(3);
+		
+		System.out.println("Size (Should be 3) = " + q.size());
+		
+		System.out.println("Polling top value (Should be 1) " + q.peek());
+		System.out.println("Removing top value (Should be 1) " + q.dequeue());
+		System.out.println("Polling top value (Should be 2) " + q.peek());
+		System.out.println("Removing top value (Should be 2) " + q.dequeue());
+		
+		System.out.println("Size (Should be 1) = " + q.size());
+		
+		
+		q.enqueue(2);
+		q.enqueue(3);
+		q.enqueue(4);
+		q.enqueue(5);
+		q.enqueue(6);
+		q.enqueue(7);
+		q.enqueue(8);
+		q.enqueue(9);
+		q.enqueue(10);
+		
+		System.out.println("Size (Should be 10) = " + q.size());
+		System.out.println("Q is full (should be true) " + q.isFull());
+		System.out.println(q);
 	}
 	
 }
